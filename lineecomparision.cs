c3e0e1300195d0bb8030b7a30e60d270e0a7ea16
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-29 12:56:09 +0530

    Added file of UC1_LINE_COMPARISION

---
+++ lineecomparision.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    class lineecomparision
+    {
+        public static void Comparision()
+        {
+            int x1, x2, y1, y2;
+            Console.WriteLine("Enter the value of x1:");      //enter the input x1
+            x1 = Convert.ToInt32(Console.ReadLine());     //Convert into Int with the help of convert.ToInt32() 
+            Console.WriteLine("Enter the value of x2:");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y1:");      //Enter the value y1
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y2:");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            double Length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)); //apply formula
+
+            Console.WriteLine("the length of a line is  " + Length);  //finally print output
+            Console.Read();
+        }
+    }
+}
