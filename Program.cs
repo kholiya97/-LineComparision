d41c2f357f80f1de2b2625860aaa8887c77cb348
kholiya97 <80846923+kholiya97@users.noreply.github.com>
2021-04-29 18:31:12 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,68 +4,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LineComparision
+namespace LineComparison
 {
-    class LineGreaterOrLess
+    class Program
     {
-      public static void UC3()
+        static void Main(string[] args)
         {
-            int x1, x2, y1, y2;
+             double  x1, x2, y1, y2;
 
-            Console.WriteLine("Enter co-ordinates of First Line ");
-            Console.WriteLine("Enter the value of x1:");      //enter the input x1
-            x1 = Convert.ToInt32(Console.ReadLine());         //Convert into Int with the help of convert.ToInt32() 
-            Console.WriteLine("Enter the value of x2:");
-            x2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the value of y1:");      //Enter the value y1
-            y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the value of y2:");
-            y2 = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Enter co-ordinates of First Line ");
+             Console.WriteLine("Enter the value of x1:");      //enter the input x1
+             x1 = Convert.ToDouble(Console.ReadLine());         //Convert into Int with the help of convert.ToInt32() 
+             Console.WriteLine("Enter the value of x2:");
+             x2 = Convert.ToDouble(Console.ReadLine());
+             Console.WriteLine("Enter the value of y1:");      //Enter the value y1
+             y1 = Convert.ToDouble(Console.ReadLine());
+             Console.WriteLine("Enter the value of y2:");
+             y2 = Convert.ToDouble(Console.ReadLine());
 
-            double Length1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)); //apply formula
+             double Length1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)); //apply formula
 
-            Console.WriteLine("Enter co-ordinates of Second Line ");
-            Console.WriteLine("Enter the value of x1:");      //enter the input x1
-            x1 = Convert.ToInt32(Console.ReadLine());         //Convert into Int with the help of convert.ToInt32() 
+             Console.WriteLine("Enter co-ordinates of Second Line ");
+             Console.WriteLine("Enter the value of x1:");      //enter the input x1
+             x1 = Convert.ToDouble(Console.ReadLine());         //Convert into Int with the help of convert.ToInt32() 
+             Console.WriteLine("Enter the value of x2:");
+             x2 = Convert.ToDouble(Console.ReadLine());
+             Console.WriteLine("Enter the value of y1:");      //Enter the value y1
+             y1 = Convert.ToDouble(Console.ReadLine());
+             Console.WriteLine("Enter the value of y2:");
+             y2 = Convert.ToDouble(Console.ReadLine());
 
-    class lineecomparision
-    {
-        public static void Comparision()
-        {
-            int x1, x2, y1, y2;
-            Console.WriteLine("Enter the value of x1:");      //enter the input x1
-            x1 = Convert.ToInt32(Console.ReadLine());     //Convert into Int with the help of convert.ToInt32() 
-            Console.WriteLine("Enter the value of x2:");
-            x2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the value of y1:");      //Enter the value y1
-            y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the value of y2:");
-            y2 = Convert.ToInt32(Console.ReadLine());
+             double Length2 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)); //apply formula
+             int compare = Length1 .CompareTo(Length2 ); //use compareto method
+             Boolean equal = Length1 .Equals(Length2 );
 
-            double Length2 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)); //apply formula
 
-            if (Length1.Equals(Length2))
+            if (Length1.CompareTo(Length2) == 0)
             {
-                Console.WriteLine("Length of Line1 = Length of Line2 ");
-            }
+                // Console.WriteLine("By Comparing the Both Lines are Same");
 
-            else if (Length1 > Length2)
-            {
-                Console.WriteLine("Length of Line1  is Greater Than Length of Line2");
+                if (Length1.Equals(Length2) == true)
+                {
+                    Console.WriteLine("By Comparing the Both Lines are Same");
+
+                    Console.WriteLine("True");
+                }
+                //else
+               // {
+
+                  //  Console.WriteLine("False");
+               // }
+
             }
             else
             {
-                Console.WriteLine("Length of Line1  is Less Than Length of Line2");
+                Console.WriteLine("Lines are not equal");
             }
 
-            Console.WriteLine("The Length of Line1 is  " + Length1);  //finaly print output
-            Console.WriteLine("The Length of Line2 is  " + Length2);  //finaly print output
+               Console.WriteLine("The Length of Line1 is  " + Length1);  //finaly print output
+               Console.WriteLine("The Length of Line2 is  " + Length2);  //finaly print output
+               Console.Read();
 
-            double Length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)); //apply formula
-
-            Console.WriteLine("the length of a line is  " + Length);  //finally print output
-            Console.Read();
-        }
-    }
-}
-
+         }
+     }
+ }
+ 
+           
